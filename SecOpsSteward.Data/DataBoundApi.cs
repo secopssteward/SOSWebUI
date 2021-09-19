af1d89cb17bf5f9a83b6ee7bbe0f60318680d139
Anthony Turner <225599+anthturner@users.noreply.github.com>
2021-09-19 14:57:55 -0400

    fix role storage, adjust demo mode

--- SecOpsSteward.Data/DataBoundApi.cs
+++ SecOpsSteward.Data/DataBoundApi.cs
@@ -42,6 +42,7 @@ namespace SecOpsSteward.Data
             {
                 UserId = tokenOwner.UserId.Id,
                 DisplayName = tokenOwner.Name,
+                Role = role,
                 Username = string.Join(',', tokenOwner.Aliases.Append(tokenOwner.Email))
             };
 
